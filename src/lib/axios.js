88b30d3bee962898a16c1287fc4fd384f59933ca
Faizal Shaikh <webworksbyfaiz@gmailcom>
2024-12-16 01:09:32 +0530

    changed some urls

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chat-application-paoz.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-application-backend-tb0f.onrender.com/api",
   withCredentials: true,
 });
